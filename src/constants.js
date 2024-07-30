44857296949c8c67b793fa9c4079169ab45eb18a
Cybercolo <coni.pya@gmail.com>
2024-07-31 01:18:19 +0200

    modifications and updated links on text windows

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,22 @@
 export const scaleFactor = 4;
 
 export const dialogueData = {
-    
+    pc: `This is my PC. I work mostly in JavaScript/React/SQL these days.
+    this is my first game in this language. I also like Vue and Node. Anyway regardless of the language, I like programming and learning.
+    Here is my <a href="https://github.com/Cybercolo" target="_blank">Github</a>!`,
+  "certifications": `This are my certifications. I hung it on the wall because I'm proud of them :) i study front-end development, IT support systems by Google and Medix rapid and intermediate Certifications.`,
+  "sofa-table": `That's my sofa. I like to relax here and watch YouTube or Netflix
+  I also write an IT blog. Go read it here <a href="https://devj101.blogspot.com" target="_blank">My Blog/a>! (If you like the content)`,
+  tv: `That's my TV. I've been watching tech youtubers a lot recently like :
+   <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
+  <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
+  bed: `This where I sleep and mediate. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it in my notes or else I won't be able to sleep because i get so excited about new projects and how to build them.`,
+  resume: `This is my desk and on it is my resume. <a href="https://github.com/JSLegendDev/Resume/blob/main/JSLegend%20Resume-1.pdf" target="_blank">Check it out?</a>
+  Contact me at jslegend@protonmail.com if you have any interesting job opportunities!`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
+  Kaboom.js by watching some of my tutorials <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
+  library: `There are a lot of programming books on my shelves. There even one in French (I also speak French btw).
+  I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
+  exit: `If you want to exit JSLegendDev's portfolio, just close the tab.`, 
 }
