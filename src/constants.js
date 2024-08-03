a5bd38f4b1f393e34fb0fdcb3d77bb7b9bfe5dba
Cybercolo <coni.pya@gmail.com>
2024-08-03 04:25:15 +0200

    1

--- src/constants.js
+++ src/constants.js
@@ -1,22 +1,18 @@
 export const scaleFactor = 4;
 
 export const dialogueData = {
-    pc: `This is my PC. I work mostly in JavaScript/React/SQL these days.
-    this is my first game in this language. I also like Vue and Node. Anyway regardless of the language, I like programming and learning.
+  pc: `This is my PC. I work mostly in JavaScript/React/SQL these days.
+    this is my first game with KaboomJs. I also like Vue and Node and i have an intermediate certification on Mendix. Anyway regardless of the language, I like programming and learning.
     Here is my <a href="https://github.com/Cybercolo" target="_blank">Github</a>!`,
-  "certifications": `This are my certifications. I hung it on the wall because I'm proud of them :) i study front-end development, IT support systems by Google and Medix rapid and intermediate Certifications.`,
-  "sofa-table": `That's my sofa. I like to relax here and watch YouTube or Netflix
-  I also write an IT blog. Go read it here <a href="https://devj101.blogspot.com" target="_blank">my channel</a>! (If you like the content)
-  You'll learn how I built this portfolio you're currently playing through!`,
-  tv: `That's my TV. I've been watching tech youtubers a lot recently like :
-   <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
-  <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
-  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  "certifications": `This are my certifications. I hung it on the wall because I'm proud of them :) i study front-end development, IT support systems by Google and Mendix rapid and intermediate Certifications.`,
+  "sofa-table": `That's my sofa. I like to relax here and watch YouTube or Netflix.
+  I also write an IT blog. Go read it here <a href="https://devj101.blogspot.com" target="_blank">my IT Blog</a>! `,
+  tv: `That's my TV. I've been watching tech youtubers a lot recently`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. I also Meditate here when im too tired to sit on my cushion.`,
   resume: `This is my desk and on it is my resume. <a href="https://drive.google.com/file/d/1Y1aC9DjJn2bs9MyiscwDWRYJuRUgm9jA/view?usp=share_link" target="_blank">Check it out!</a>
   Contact me at coni.pya@gmail.com if you have any interesting job opportunities!`,
   projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
   Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.`,
-  library: `There are a lot of programming books on my shelves. There even one in French (I also speak French btw).
-  I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
+  library: `There are a lot of programming books on my shelves. There even one in German & French (I speak both languages).`,
   exit: `If you want to exit JSConiDev's portfolio, just close the tab.`, 
 }
